9e037a549a4195abe8f8d9b32425bafddbce2289
ashwganes <ashwing274@gmail.com>
2018-08-21 21:50:57 +0530

    Initial project commit

---
+++ jvs-core-api/src/main/java/com/sample/jvs_core_api/App.java
@@ -0,0 +1,13 @@
+package com.sample.jvs_core_api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
